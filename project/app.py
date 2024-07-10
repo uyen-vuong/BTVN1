b0190210aef0047a1b970aedf01c4756f6ba2e10
uyen-vuong <122289264+uyen-vuong@users.noreply.github.com>
2024-07-10 10:24:05 +0700

    Update app.py

--- project/app.py
+++ project/app.py
@@ -6,6 +6,4 @@ app = FastAPI()
 app.include_router(image_router, prefix="/image", tags=["Image Manipulation"])
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
